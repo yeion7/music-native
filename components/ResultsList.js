06074a79bd46c108f30f3484c435c33b7cfa47ad
Yeison Daza <jahir045@gmail.com>
2017-05-13 14:10:35 -0500

    [update] result list component

--- components/ResultsList.js
+++ components/ResultsList.js
@@ -9,26 +9,25 @@ import SongItem from "./SongItem";
 import AlbumItem from "./AlbumItem";
 import { error } from "../lib/error";
 
-export default function ResultsList(props) {
-  const { tracks, albums, handlePress } = props;
-  return (
-    <View>
-      <Separator>Canciones</Separator>
-      <List
-        dataArray={tracks}
-        renderRow={info => <SongItem {...info} handlePress={handlePress} />}
-      />
-      <Separator>Álbumes</Separator>
-      <List
-        dataArray={albums}
-        renderRow={info => <AlbumItem {...info} handlePress={handlePress} />}
-      />
-    </View>
-  );
-}
+const ResultsList = ({ tracks, albums, handlePress }) => (
+  <View>
+    <Separator>Canciones</Separator>
+    <List
+      dataArray={tracks}
+      renderRow={info => <SongItem {...info} handlePress={handlePress} />}
+    />
+    <Separator>Álbumes</Separator>
+    <List
+      dataArray={albums}
+      renderRow={info => <AlbumItem {...info} handlePress={handlePress} />}
+    />
+  </View>
+);
 
 const Separator = styled.Text`
   background-color: #f0eff5;
   padding: 10;
   font-weight: 600;
 `;
+
+export default ResultsList;
